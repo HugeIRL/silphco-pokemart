4c7bc9e1438be1b2088ab211d23bc7ed33c04c7e
HugeIRL <codepirate@icloud.com>
2021-03-19 12:34:00 -0500

    add active storage and update Creature model to include image

--- app/models/creature.rb
+++ app/models/creature.rb
@@ -3,4 +3,6 @@ class Creature < ApplicationRecord
 
   validates :pokedex_id, :species, :description, :price_cents, presence: true
   validates :pokedex_id, :price_cents, numericality: { only_integer: true }
+
+  has_one_attached :image
 end
