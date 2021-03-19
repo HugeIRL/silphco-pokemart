f0b6f2ca3b3a156fd86069671836477387f15049
HugeIRL <codepirate@icloud.com>
2021-03-19 12:37:48 -0500

    add database seed with testing data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "rest-client"
+
+Type.delete_all
+Creature.delete_all
+
+pokemon = []
+description = []
+
+# limit the amount of Pokemon to 25 for now. End goal is to get the original 151.
+(1..25).each do |i|
+  data = RestClient.get("https://pokeapi.co/api/v2/pokemon/#{i}")
+  json_data = JSON.parse(data)
+  pokemon.push(json_data)
+  puts "GET data for Pokemon id #{i}"
+
+  desc = RestClient.get("https://pokeapi.co/api/v2/pokemon-species/#{i}")
+  json_desc = JSON.parse(desc)
+  description.push(json_desc)
+end
+
+pokemon.each do |creature|
+  type = Type.find_or_create_by(name: creature["types"][0]["type"]["name"])
+
+  if type && type.valid?
+    p = type.creatures.create(
+      pokedex_id:  creature["id"],
+      species:     creature["name"],
+      description: description[creature["id"] - 1]["flavor_text_entries"][description[creature["id"] - 1]["flavor_text_entries"].length - 1]["flavor_text"],
+      price_cents: rand(5000..100_000).to_i
+    )
+    puts "Creating #{p.species} with type #{p.type.name}"
+
+    downloaded_image = URI.open("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/#{p.id}.png")
+    p.image.attach(io: downloaded_image, filename: "m-#{p.id}.png")
+    puts "Downloaded image for #{p.species}" if downloaded_image
+  else
+    puts "Invalid type #{p['type']} for pokemon #{p['species']}"
+  end
+end
+
+puts "Created #{Type.count} types."
+puts "Created #{Creature.count} pokemon."
