6a39f88b50f359bb6830324c3b88fac089decadd
HugeIRL <codepirate@icloud.com>
2021-03-20 11:28:14 -0500

    update application controller for correct devise registration fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,21 @@ class ApplicationController < ActionController::Base
   def cart
     Creature.find(session[:shopping_cart])
   end
+
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:first_name, :last_name, :address, :postal_code, :email, :password)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:first_name, :last_name, :address, :postal_code, :email, :password,
+               :current_password)
+    end
+  end
 end
