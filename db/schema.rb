e7f2c23a14e7fa990f0bf316c920e2a3f8b32161
HugeIRL <codepirate@icloud.com>
2021-03-20 11:25:06 -0500

    add devise gem for user login and registration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_20_142441) do
+ActiveRecord::Schema.define(version: 2021_03_20_161921) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
