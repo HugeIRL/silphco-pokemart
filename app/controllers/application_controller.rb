ac6d25632452e0fa9697b898e68b08dccc634a93
HugeIRL <codepirate@icloud.com>
2021-03-21 15:27:07 -0500

    add City to User, registration process and Active Admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,11 +20,12 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:first_name, :last_name, :address, :province_id, :postal_code, :email, :password)
+      u.permit(:first_name, :last_name, :address, :city_id, :province_id, :postal_code, :email,
+               :password)
     end
 
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:first_name, :last_name, :address, :province_id, :postal_code, :email, :password,
+      u.permit(:first_name, :last_name, :address, :city_id, :province_id, :postal_code, :email, :password,
                :current_password)
     end
   end
