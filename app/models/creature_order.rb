ba17b653ea2dabe97ad6e94b99ba00ccfe508a29
HugeIRL <codepirate@icloud.com>
2021-03-24 10:14:17 -0500

    add validation to CreatureOrder model

--- app/models/creature_order.rb
+++ app/models/creature_order.rb
@@ -1,4 +1,7 @@
 class CreatureOrder < ApplicationRecord
   belongs_to :order
   belongs_to :creature
+
+  validates :quantity, presence: true
+  validates :quantity, numericality: { only_integer: true }
 end
