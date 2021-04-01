82425272bc16e7485c0a3ad6092cbed2982ca61f
HugeIRL <codepirate@icloud.com>
2021-04-01 07:39:48 -0500

    add Type badges to all views containing Creatures

--- app/models/type.rb
+++ app/models/type.rb
@@ -6,4 +6,12 @@ class Type < ApplicationRecord
   def capitalized_name
     name.capitalize
   end
+
+  def badge(type)
+    @badge = if !Rails.application.assets.find_asset("#{type}.png").nil?
+               "#{type}.png"
+             else
+               "unknown.png"
+             end
+  end
 end
