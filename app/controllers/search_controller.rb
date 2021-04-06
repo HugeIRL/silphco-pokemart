1ae1b9a4cce3267c57a3343f09a25d1b7157e470
HugeIRL <codepirate@icloud.com>
2021-04-06 15:48:10 -0500

    update search controller for better handling

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,20 +1,44 @@
 class SearchController < ApplicationController
   def search
-    redirect_to(root_path) if params[:species].blank?
-    @results = change_output(params[:species], params[:type]).page(params[:page])
-    redirect_to(root_path) and return if @results.blank?
+    results = change_output(params[:species], params[:type])
+    validate(results)
   end
 
   def change_output(species, type)
-    return unless species.downcase.nil? == false
+    if species.blank? && type.blank?
+      nil
+    elsif species.blank?
+      display_by_type(type)
+    elsif type.blank?
+      display_by_name_or_desc(species)
+    else
+      display_by_any(species, type)
+    end
+  end
 
+  def display_by_type(type)
     Creature.joins(:type).all.where("lower(name) LIKE :type",
                                     type: "%#{type}%")
+            .page(params[:page])
+  end
 
-    return unless type.downcase.nil? == false
+  def display_by_name_or_desc(species)
+    @results = Creature.joins(:type).all.where("lower(species) LIKE :species OR
+    lower(description) LIKE :species",
+                                               species: "%#{species}%")
+                       .page(params[:page])
+  end
 
-    Creature.joins(:type).all.where("lower(species) LIKE :species OR
-                                          lower(description) LIKE :species",
-                                    species: "%#{species}%")
+  def display_by_any(species, type)
+    Creature.joins(:type).all.where("lower(species) LIKE :species AND
+    lower(name) LIKE :type",
+                                    species: "%#{species}%",
+                                    type:    "%#{type}%")
+            .page(params[:page]).per(6)
+  end
+
+  def validate(results)
+    message = "No result found for '#{params[:species]}' with type: '#{params[:type]}'"
+    redirect_to(root_path) and flash[:alert] = message if results.blank?
   end
 end
