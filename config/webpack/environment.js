8de7806648f39fa21764a0f3d3a76de87588ffee
HugeIRL <38887481+HugeIRL@users.noreply.github.com>
2022-10-15 13:17:10 -0500

    fix bootstrap and initialization on Apple Silicon

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,16 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
 
-module.exports = environment
+module.exports = environment;
 
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-new webpack.ProvidePlugin({
-  $: 'jquery/src/jquery',
-  jQuery: 'jquery/src/jquery'
-}))
+const webpack = require("webpack");
+
+environment.plugins.append(
+	"Provide",
+	new webpack.ProvidePlugin({
+		$: "jquery",
+		jQuery: "jquery",
+		Popper: ["popper.js", "default"], // Not a typo, we're still using popper.js here
+	})
+);
+
+module.exports = environment;
